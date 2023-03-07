e66fe184bee020f96f0af545e238f41325eb3c01
alanz420 <lucasalonso.m94@gmail.com>
2023-03-07 13:49:54 -0300

    agregue rutas de registro, login y show

--- routes/routeUsers.js
+++ routes/routeUsers.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const {
+  registerController,
+  showAll,
+  loginUser,
+  deleteUser,
+} = require("../controllers/controllerUsers");
 
 const router = express.Router();
+router.get("/show", showAll);
+router.post("/register", registerController);
+router.post("/login", loginUser);
+router.delete("/delete", deleteUser);
 
 module.exports = router;
